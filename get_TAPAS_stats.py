62afbf02d294e3e93d00e1251673960cf0d9c459
Imre Kertesz <ikertesz@biomed.ee.ethz.ch>
2025-03-20 12:00:33 +0100

    update script

--- get_TAPAS_stats.py
+++ get_TAPAS_stats.py
@@ -1,7 +1,6 @@
 import requests
 import json
 import os
-from datetime import datetime
 
 GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
 GIST_ID = os.getenv("GIST_ID")
@@ -35,26 +34,25 @@ total_stars = sum(repo['stargazers_count'] for repo in repos)
 total_forks = sum(repo['forks_count'] for repo in repos)
 total_issues = sum(repo['open_issues_count'] for repo in repos)
 
-current_date = datetime.now().strftime('%Y-%m-%d')
-
 for repo in repos:
     repo_name = repo['name']
+
+    clones_count_old = 0
+    clones_unique_old = 0
     
-    # Check if the current date is already in the accumulated data
+    # Check if there is existing data
     if repo_name in accumulated_data:
-        dates = [entry['date'] for entry in accumulated_data[repo_name]]
-        if current_date in dates:
-            print(f"Repo: {repo_name} - Entry for {current_date} already exists, skipping.")
-            continue
+        repo_data = accumulated_data[repo_name][0]
+        clones_count_old = repo_data['clones_all']
+        clones_unique_old = repo_data['clones_unique']
     
     traffic_url = f"https://api.github.com/repos/{org_name}/{repo_name}/traffic/clones"
     traffic_response = requests.get(traffic_url, headers=headers)
-
-    #print(f"Status Code: {traffic_response.status_code}")
     
     if traffic_response.status_code == 200:
         clones_data = traffic_response.json()
         clones_count = clones_data['count']  # Total number of times the repo was cloned
+        clones_unique = clones_data['uniques'] # Unique number of clones
     else:
         clones_count = 0
         print(f"Repo: {repo_name} - Clones data unavailable (requires admin access)")
@@ -72,14 +70,16 @@ for repo in repos:
     else:
         print(f"Repo: {repo_name} - Release data unavailable (requires admin access)")
 
-    # Update the accumulated data
-    if repo_name not in accumulated_data:
-        accumulated_data[repo_name] = []
+    # remove old data
+    accumulated_data[repo_name] = []
     
     accumulated_data[repo_name].append({
-        'date': current_date,
-        'clones': clones_count,
-        'downloads': download_count
+        'clones_all': clones_count+clones_count_old,
+        'clones_unique': clones_unique+clones_unique_old,
+        'downloads': download_count,
+        'stargazers': repo['stargazers_count'],
+        'forks': repo['forks_count'],
+        'open_issues': repo['open_issues_count']
     })
 
 # Update the Gist with the new data
@@ -91,8 +91,6 @@ update_gist_data = {
         }
     }
 }
-
-# print(f"Json: {json.dumps(update_gist_data)}")
 
 update_response = requests.patch(gist_url, headers=headers, data=json.dumps(update_gist_data))
 
